34c8da242a2c72545943993ee24d85b6514f3803
fernahh <fernahh@gmail.com>
2017-02-26 21:21:11 -0300

    refactor(page): rename base layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,6 @@
-import Application from '../layouts/application'
+import Layout from '../layouts/layout'
 
 export default () => (
-  <Application>
-    <h1>fernahh.com.br</h1>
-    <p>
-      Lorem ipsum dolor sit amet, <a href="#">adipisicing elit</a> , sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-      qui officia deserunt mollit anim id est laborum.
-    </p>
-  </Application>
+  <Layout>
+  </Layout>
 )
