2918a5044021e368b57c7706cc7647009241c8ac
fernahh <fernahh@gmail.com>
2017-02-26 21:21:54 -0300

    feat(navigation): add navigation links array

---
+++ data/navigationLinks.js
@@ -0,0 +1,21 @@
+const navigationLinks = [
+  {
+    href: '/sobre',
+    name: 'Sobre',
+    title: 'Quem é @fernahh?'
+  }, {
+    href: '/talks',
+    name: 'Talks',
+    title: 'Página com lista de palestras'
+  }, {
+    href: 'https://twiter.com/fernahh',
+    name: 'Twitter',
+    title: 'Link para o Twitter @fernahh'
+  }, {
+    href: 'https://github.com/fernahh',
+    name: 'Github',
+    title: 'Link para projetos no Github'
+  }
+]
+
+export default navigationLinks
