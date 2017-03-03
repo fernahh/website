b7975fbfe1f802c0e8dc2aaeab260490a2214cbc
fernahh <fernahh@gmail.com>
2017-03-02 21:34:44 -0300

    feat(post-index): add post index component

---
+++ components/post-index.js
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import colors from '../theme/styles'
+import HR from './hr'
+import Time from './time'
+
+const PostIndex = ({ post }) => (
+  <li>
+    <Time date={post.date}/>
+
+    <h2>
+      <Link href={post.path} as={post.id} prefetch>
+        <a>{post.title}</a>
+      </Link>
+    </h2>
+
+    <p>{post.description}</p>
+
+    <HR />
+
+    <style jsx>{`
+      li {
+        margin-bottom: 2em;
+      }
+
+      h2 {
+        margin-top: 0;
+        margin-bottom: 20px;
+        font-size: 2em;
+        font-weight: 600;
+        line-height: 1.2em;
+        letter-spacing: -1.79px;
+        transition: all 200ms ease-in-out;
+      }
+
+      a {
+        color: ${colors.adjacent};
+        border: 0;
+      }
+
+      a:hover,
+      a:active,
+      a:focus {
+        color: ${colors.black};
+      }
+    `}</style>
+  </li>
+)
+
+export default PostIndex
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,32 @@
+import Link from 'next/link'
 import Layout from '../layouts/layout'
+import posts from '../data/posts'
+import colors from '../theme/styles'
+import PostIndex from '../components/post-index'
+
+const getPosts = () => posts.map(post => Object.assign({
+  path: `/posts/${post.id}`
+}, post))
 
 export default () => (
   <Layout>
+    <ul>
+      {
+        getPosts().map(post => (
+          <div>
+            <PostIndex key={post.id} post={post}/>
+          </div>
+        ))
+      }
+    </ul>
+
+    <style jsx>{`
+      ul {
+        display: block;
+        max-width: 620px;
+        margin: auto;
+        list-style-type: none;
+      }
+    `}</style>
   </Layout>
 )
