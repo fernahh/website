d767c3432cf73186a4c994248f07e857ea38ff3c
fernahh <fernahh@gmail.com>
2017-03-05 12:19:52 -0300

    docs(about): add section about me

---
+++ pages/sobre.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Article from '../layouts/article'
+import Heading from '../components/heading'
+
+export default () => (
+  <Article>
+    <Heading>Sobre</Heading>
+
+    <p>E aí, tudo certo?</p>
+
+    <p>Me chamo Luiz Fernando Rodrigues, mais conhecido como <em>"Fernahh"</em>.</p>
+
+    <p>Nasci em Sobradinho, interior do Rio Grande do Sul. Morei cinco anos em
+      Canela, onde me formei em Análise e Desenvolvimento de Sistemas,
+      pela <Link href="http://www.ucs.br/site">Universidade de Caxias do Sul</Link>.</p>
+
+    <p>Trabalho e vivo da web desde 2010. Já contribui com projetos escritos em
+      PHP, Ruby e JavaScript. Além disso, busco compartilhar meus conhecimentos
+      através de palestras, artigos e projetos open source.</p>
+
+    <p>Atualmente trabalho como engenheiro de software na <Link href="https://contaazul.com/">
+      ContaAzul</Link>, com o principal foco em desenvolvimento front-end.</p>
+
+    <p>Se quiser entrar em contato, podemos conversar através do
+      e-mail <Link href="mailto:fernahh@gmail.com">fernahh@gmail.com</Link>.</p>
+  </Article>
+)
