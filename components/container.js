b0e5feb990807056cf184afdcad357b27c424d97
fernahh <fernahh@gmail.com>
2017-03-19 11:10:50 -0300

    fix(container): fix container sizes

--- components/container.js
+++ components/container.js
@@ -6,6 +6,9 @@ export default ({ children }) => (
       div {
         max-width: 620px;
         margin: 0 auto;
+        padding-left: 10px;
+        padding-right: 10px;
+        overflow: hidden;
       }
     `}</style>
   </div>
